1738e8c30bac08e914b1494ceb7ddb71aa9bd4e0
Your Name <your@email.com>
2026-05-07 14:42:42 +0300

    Add Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+	<>
+		<App />
+		<SpeedInsights />
+	</>
+);
 
 if ("serviceWorker" in navigator && import.meta.env.PROD) {
 	window.addEventListener("load", () => {
